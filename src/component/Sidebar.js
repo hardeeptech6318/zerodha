ef55a52297ad86c58705fe91f12cb52b277a27bb
hardeepsinh parmar <hardeepsinhparmar2020@gmail.com>
2023-05-18 22:22:45 +0530

    sidebar dropdown working

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -4,6 +4,17 @@ import React   from 'react'
 
 function Sidebar() {
 
+  const showmarketdepth =(item,i)=>{
+    
+    let id = document.getElementsByClassName(`showmarketdepth${i}`)[0].style.display;
+    console.log(id);
+    if(id=="none"){
+      document.getElementsByClassName(`showmarketdepth${i}`)[0].style.display="block"
+    }else{
+      document.getElementsByClassName(`showmarketdepth${i}`)[0].style.display="none"
+    }
+ }
+
 let data =   
   
  
@@ -68,7 +79,7 @@ const onleave=(item,i)=>{
 
 
   return (
-    <div className='m-0 p-0  ' >
+    <div className='m-0 p-0 sidebartextsize '  >
       <div className='border-bottom d-flex w-100'>
       <label className="searchicon  p-3  " ><i className=" bi bi-search"></i></label>
   <input className="sidebarsearch " placeholder="Search eg:infy,bse,nifty fut, nifty weekly,gold mcx" type="text" />
@@ -78,15 +89,15 @@ const onleave=(item,i)=>{
 
       </div>
        
-       <div>
+       <div >
        {data.map((ele,i)=>{
       
       
       return<>
-      <div key={i} onMouseLeave={()=>onleave(ele,i)}  onMouseEnter={()=>onenter(ele,i)} className={`sidebardata${i}   position-relative d-flex justify-content-between py-3 bg-light border-bottom px-1 `}>
+      <div key={i} onMouseLeave={()=>onleave(ele,i)}  onMouseEnter={()=>onenter(ele,i)} className={`sidebardata${i}    position-relative d-flex justify-content-between   border-bottom padding12 `}>
         
         <div >{ele.tradingsymbol}</div>
-        <div className='d-flex '>
+        <div className='d-flex  '>
           <div>
             <div>
               <span>-2.35</span>
@@ -98,14 +109,19 @@ const onleave=(item,i)=>{
             <span>529.6</span>
           </div>
         </div>
-        <div className={` hoverbutton${i} hidemenubutton   position-absolute`}>
-          <button>b</button>
-          <button>s</button>
-          <button>g</button>
-          <button>b</button>
-          <button>o</button>
+        <div className={` hoverbutton${i} hidemenubutton end-0 top-8  showmenu   position-absolute`}>
+            <button className='hoverbuttondisign mx-1' >B</button>
+            <button className='hoverbuttondisign ' >S</button>
+            <button className='hoverbuttondisign mx-1' onClick={()=>showmarketdepth(ele,i)}  ><i class="bi bi-text-center"></i></button>
+            <button className='hoverbuttondisign ' ><i class="bi bi-graph-up-arrow"></i></button>
+            <button className='hoverbuttondisign mx-1' ><i class="bi bi-three-dots"></i></button>
         </div>
+
+       
       </div>
+      <div className={`showmarketdepth${i}`} style={{display:"none"}} >
+          {ele.tradingsymbol}
+        </div>
       </>
     })}
        </div>
